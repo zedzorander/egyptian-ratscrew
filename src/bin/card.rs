8a0f11e8ff913cf2cd00253828338272af018cec
zedzorander <cophares@pdx.edu>
2018-06-06 10:51:52 -0700

    cleaned up card.rs

--- src/bin/card.rs
+++ src/bin/card.rs
@@ -6,11 +6,6 @@
 // Code to create a shuffled deck of cards borrowed and modified from
 // http://cultofmetatron.io/2017/03/21/learning-rust-with-blackjack-part-1/
 
-//extern crate serde;
-//use serde::ser::{Serialize, Serializer, SerializeStruct};
-//use serde::{Deserialize, Deserializer};
-//use serde::de::EnumAccess;
-
 /// Suit of the card
 #[derive(Debug, Clone, Copy, Eq)]
 pub enum Suit {
@@ -40,27 +35,6 @@ impl PartialEq<Suit> for Suit {
     }
 }
 
-
-/*
-/// Serializer for Suit
-impl Serialize for Suit {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer, {
-        let mut state = serializer.serialize_struct("Suit", 1)?;
-        state.serialize_field("suit", &self)?;
-        state.end()
-    }
-}
-
-/// Deserializer for Suit
-impl<'de> Deserialize<'de> for Suit {
-    fn deserialize<D>(deserialize: D) -> Result<Self, D::Error>
-        where D: Deserializer<'de>
-    {
-        Ok(
-            let s: Suit
-*/
-
 /// enum for card value
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Rank {
@@ -85,29 +59,6 @@ impl Rank {
         }
     }
 }
-/*
-/// Serializer for Rank
-impl Serialize for Rank {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer, {
-        let mut state = serializer.serialize_struct("Rank", 1)?;
-        state.serialize_field("rank", &self.value())?;
-        state.end()
-    }
-}
-
-/// Deserializer for Rank
-impl<'de> Deserialize<'de> for Rank {
-    fn deserialize<D>(deserialize: D) -> Result<Self, D::Error>
-        where D: Deserializer<'de>
-    {
-        let helper = Helper::deserialize(deserializer)?;
-        
-            
-        //??? = helper.rank;
-    }
-}
-*/
 
 /// Struct for playing card
 #[derive(Debug, Clone, Copy, Eq)]
@@ -162,32 +113,7 @@ impl Card {
         }
     }
 }
-/*
-/// Serializer for Card
-impl Serialize for Card {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer, 
-    {
-        let mut state = serializer.serialize_struct("Card", 2)?;
-        state.serialize_field("rank", &self.rank)?;
-        state.serialize_field("suit", &self.suit)?;
-        state.end()
-    }
-}
 
-/// Deserializer for Card
-impl<'de> Deserialize<'de> for Card {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where D: Deserializer<'de>
-    {
-        let helper = Deserialize::deserialize(deserializer)?;
-        Ok(Card {
-            rank = helper.rank;
-            suit = helper.suit;
-        })
-    }
-}
-*/
 /// Implements display for Card struct
 impl std::fmt::Display for Card {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
